01ec311d83e324eb29cba67f648ce2a30380d338
longfeiW <longfeiwang.88@gmail.com>
2025-09-15 18:27:46 +0800

    add base testnet

--- app/providers.tsx
+++ app/providers.tsx
@@ -4,14 +4,14 @@ import { RainbowKitProvider, getDefaultConfig } from '@rainbow-me/rainbowkit';
 import '@rainbow-me/rainbowkit/styles.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { WagmiProvider } from 'wagmi';
-import { avalancheFuji, filecoinCalibration } from 'wagmi/chains';
+import { avalancheFuji, filecoinCalibration,baseSepolia } from 'wagmi/chains';
 
 const queryClient = new QueryClient();
 
 const config = getDefaultConfig({
   appName: 'OnRamp',
   projectId: 'WALLETCONNECT_PROJECT_ID',
-  chains: [avalancheFuji, filecoinCalibration],
+  chains: [avalancheFuji, filecoinCalibration,baseSepolia],
   ssr: true, // If your dApp uses server side rendering (SSR)
 });
 
